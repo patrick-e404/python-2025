6911510e24a12e3a42794a9281a2cb9f5f348833
patrick-e404 <patrick_e404@hotmail.com>
2025-11-30 12:51:58 -0300

    site para busca de cep

---
+++ APP/app_cep.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+URL = "https://viacep.com.br/ws/{cep}/json"
+
+st.title("Busca CEP")
+cep = st.text_input("Busque seu CEP")
+
+if cep != "":
+    try:
+        resp = requests.get(URL.format(cep=cep))
+        data = pd.DataFrame([resp.json()])
+        st.dataframe(data, hide_index=True)
+
+    except Exception as err:
+        st.error("Entre com um CEP válido!")
