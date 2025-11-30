af61518e611db3e6faa6677ef28d4d5c316a39dc
patrick-e404 <patrick_e404@hotmail.com>
2025-11-30 12:34:59 -0300

    finished course

--- API/01_cep.py
+++ API/01_cep.py
@@ -1,6 +1,8 @@
 #%%
 import requests
 import json
+from tqdm import tqdm
+import pandas as pd
 
 ceps = [
     "95034160",
@@ -13,13 +15,17 @@ ceps = [
 
 url = "https://viacep.com.br/ws/{cep}/json"
 dados = []
-for i in ceps:
+for i in tqdm(ceps):
     res = requests.get(url.format(cep=i))
     if res.status_code == 200:
         dados.append(res.json())
 
-
 dados
+#%%
 
+dataset = pd.DataFrame(dados)
+dataset.to_csv("ceps.csv", sep=";")
+
+#%%
 with open("ceps.json", "w", encoding="utf-8") as open_file:
     json.dump(dados, open_file, ensure_ascii=False, indent=4)
